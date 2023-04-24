524634a1927bd908c933a6b1e4fe03742cedde2a
Valery <83373303+Valery-a@users.noreply.github.com>
2023-04-25 02:38:23 +0300

    Added a Lexer class
    To tokenize the input expression

--- src/main.py
+++ src/main.py
@@ -3,3 +3,29 @@ class Token:
         self.type = type
         self.value = value
 
+class Lexer:
+    def __init__(self, input):
+        self.input = input
+        self.pos = 0
+    
+    def next_token(self):
+        if self.pos >= len(self.input):
+            return Token("EOF", None)
+        
+        if self.input[self.pos].isdigit():
+            token_value = ""
+            while self.pos < len(self.input) and self.input[self.pos].isdigit():
+                token_value += self.input[self.pos]
+                self.pos += 1
+            return Token("NUMBER", int(token_value))
+        
+        elif self.input[self.pos] == "+":
+            self.pos += 1
+            return Token("PLUS", "+")
+        
+        elif self.input[self.pos] == "-":
+            self.pos += 1
+            return Token("MINUS", "-")
+        
+        else:
+            raise Exception("Invalid input")
